0dd8afda7a9681e96b7993c49fcf4b6d4441c60e
Bob Reselman <reselbob@gmail.com>
2019-11-25 14:45:16 -0800

    adding k8s support

--- docker-seed.sh
+++ docker-seed.sh
@@ -6,7 +6,7 @@ docker run -d -p 5000:5000 --restart=always --name registry registry:2
 #Create the seat-saver image
 docker build -t seatsaver .
 
-docker tag airline localhost:5000/seatsaver:beta
+docker tag seatsaver localhost:5000/seatsaver:beta
 
 docker push localhost:5000/seatsaver:beta
 
